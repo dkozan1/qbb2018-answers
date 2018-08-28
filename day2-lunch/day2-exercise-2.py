f9ffec0e0559c5d86fe41e93224881f3880c37a6
Darby Kozan <dkozan1@jh.edu>
2018-08-28 13:08:14 -0400

    Python loop using NM:i:0 to find perfect alignments

---
+++ day2-lunch/day2-exercise-2.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+#if the 3rd column is 3R/2L etc is one of those, count it
+#if it has an @, skip, don't count it
+#to find perfect alignments find NM:i:0 's in the list
+import sys
+
+if len(sys.argv) > 1:
+    f = open( sys.argv[1] )
+else:
+    f = sys.stdin
+num_perfect_alignments = 0
+
+for line in f:
+    if "NM:i:0" in line:
+        num_perfect_alignments += 1
+print(num_perfect_alignments)
